ab1c192fd2222be83cc03c18508123ba474b50c2
Filipp <philipp3gs@gmail.com>
2024-04-05 12:49:30 +0300

    added routing config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,8 @@
-import React, {Suspense} from 'react';
-import {Link, Route, Routes} from 'react-router-dom';
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import React from 'react';
+import {Link} from 'react-router-dom';
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
+import {AppRouter} from "app/providers/router";
 
 import "./styles/index.scss";
 
@@ -18,12 +17,7 @@ const App = () => {
             </button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={'Loading...'}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage/>}/>
-                    <Route path={'/'} element={<MainPage/>}/>
-                </Routes>
-            </Suspense>
+            <AppRouter/>
         </div>
     );
 };
